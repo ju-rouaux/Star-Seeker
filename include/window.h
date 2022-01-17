8b79915b484c800cc5ba5f3e66d6cf7bfd7bf9a4
Rouaux Julien <ju.rouaux@gmail.com>
2022-01-17 15:16:43 +0100

    Ajout du module de création de fenetre

---
+++ include/window.h
@@ -0,0 +1,7 @@
+#ifndef _JEU_WINDOW_
+#include <SDL2/SDL.h>
+
+int creerFenetreEtRendu(SDL_Window ** window, SDL_Renderer ** renderer);
+void detruireFenetreEtRendu(SDL_Window ** window, SDL_Renderer ** renderer);
+
+#endif
---
+++ src/window.c
@@ -0,0 +1,58 @@
+/**
+ * \file
+ * \brief Module de création de la fenêtre du jeu et son rendu
+ * \author Julien
+*/
+
+#include <SDL2/SDL.h>
+
+/**
+ * Génère une fenêtre et son rendu, pour les mettre dans 
+ * les pointeurs donnés en paramètre.
+ * 
+ * \param window L'adresse du pointeur de fenêtre
+ * \param renderer L'adresse du pointeur de rendu
+ * 
+ * \return 0 si tout s'est bien passé, sinon une valeur négative
+*/
+int creerFenetreEtRendu(SDL_Window ** window, SDL_Renderer ** renderer)
+{
+    (*window) = SDL_CreateWindow("Star Seeker", 
+                    SDL_WINDOWPOS_CENTERED, 
+                    SDL_WINDOWPOS_CENTERED,
+                    1280,
+                    720,
+                    SDL_WINDOW_RESIZABLE);
+    
+    if(*window == NULL)
+    {
+        printf("Impossible de créer la fenêtre : %s\n", SDL_GetError());
+        return -1;
+    }
+
+    (*renderer) = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+
+    if(*renderer == NULL)
+    {
+        printf("Impossible de créer le rendu : %s\n", SDL_GetError());
+        return -2;
+    }
+
+    return 0;
+}
+
+/**
+ * Libère la mémoire allouée pour la fenêtre et son rendu, et met les
+ * pointeurs à NULL.
+ * 
+ * \param window L'adresse du pointeur de fenêtre
+ * \param renderer L'adresse du pointeur de rendu
+*/
+void detruireFenetreEtRendu(SDL_Window ** window, SDL_Renderer ** renderer)
+{
+    SDL_DestroyRenderer(*renderer);
+    (*renderer) = NULL;
+
+    SDL_DestroyWindow(*window);
+    (*window) = NULL;
+}
