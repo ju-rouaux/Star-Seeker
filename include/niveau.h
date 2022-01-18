80f7bbadad71264af58db01fee62b4a3c697827b
Rouaux Julien <ju.rouaux@gmail.com>
2022-01-18 12:09:37 +0100

    Simple avancée

--- include/niveau.h
+++ include/niveau.h
@@ -2,9 +2,31 @@
  * \struct t_obstacle
  * \brief Structure représentant une salle
 */
+
+/*
+
+    Memo structure de salle
+
+    Une salle contient un identifiant et les 4 salles vers
+    lesquelles elle dirige. Une porte NULL n'est pas affichée.
+
+    Deux salles avec le même identifiant génèrent une unique salle, elles ne sont justement
+    pas reliées entre elles (pointeur à NULL).
+
+    Pour lier les salles, on regarde toute la matrice et si l'identifiant de la salle du
+    dessus ou d'à gauche, est identique on relie les salles.
+*/
+
+typedef enum {
+    UP, RIGHT, DOWN, LEFT
+} e_porte;
+
+#define NOMBRE_DE_PORTES 4 //Taille maximale d'un groupe de salle (à definir)
+
 typedef struct s_salle{
     int complete; /** Booléen, vrai si la salle est complétée (plus de monstres) */
-    struct s_salle ** groupe; /** Ensemble de salles formant une unique salle */
+    struct s_salle * portes[NOMBRE_DE_PORTES]; /** Ensemble de salles formant une unique salle (tableau de pointeurs sur salle) */
+    int id_salle;
     //t_monstre ** (liste de monstres)
     //t_obstacle ** (liste d'obstacles)
 } t_salle;
@@ -14,7 +36,7 @@ typedef struct s_salle{
  * \brief Structure représentant une matrice de salles
 */
 typedef struct {
-    t_salle *** salles; //??????????????????????? je vais voir ça
+    t_salle ** salles; 
     int h; /** Largeur du niveau */
     int l; /** Hauteur du niveau */
 } t_niveau;
--- src/niveau.c
+++ src/niveau.c
@@ -1,20 +1,106 @@
+/**
+ * \file
+ * \brief Module de chargement d'un niveau en structure interprétable pour notre moteur de rendu. 
+ * (Des optimisations doivent être faites)
+ * \author Julien
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <niveau.h>
 
+void lierSalles(t_niveau * niveau)
+{
+    int hauteur, largeur, id_courant;
+    for(int i = 0; i < niveau->h; i++)
+    {
+        for(int j = 0; j < niveau->l; j++)
+        {
+            hauteur = i * niveau->l;
+            largeur = j;
+            id_courant = niveau->salles[hauteur + largeur]->id_salle;
+            
+            if((hauteur-l) > 0 ) //Vérification des limites
+            {
+                if(niveau->salles[hauteur-l + largeur] ->) //Vérifier si la salle du dessus a le même id
+                {
+
+                }
+            }
+            if((largeur-1) > 0)
+            {
+                if(niveau->salles[hauteur + largeur-1]) //Vérifier si la salle d'à gauche a le même id
+                {
+
+                }
+                else
+                    niveau->salles
+            }
+        }
+    }
+
+}
+
+
+/**
+ * Génère une structure salle et mets les pointeurs de son groupe
+ * à NULL.
+ * 
+ * \param id_salle L'identifiant de la salle
+ * 
+ * \return Le pointeur de la salle créée, NULL si échec.
+ */
+t_salle * creerSalle(int id_salle)
+{
+    t_salle * salle = malloc(sizeof(t_salle));
+    if(salle == NULL)
+    {
+        printf("Impossible d'allouer la mémoire d'une salle.\n");
+        return NULL;
+    }
+
+    for(int i = 0; i < NOMBRE_DE_PORTES; i++)
+    {
+        salle->portes[i] = NULL;
+    }
+    salle->complete = 0;
+    salle->id_salle = id_salle;
+}
+
+/**
+ * Libère la mémoire allouée pour une salle et mets son
+ * pointeur à NULL
+ * 
+ * \param salle L'adresse du pointeur de la salle
+ */
+void detruireSalle(t_salle ** salle)
+{
+    free(*salle);
+    salle = NULL;
+}
+
 //En cours d'ecriture
 int chargerNiveau(FILE * fichier)
 {
+    //Dimensions
     int largeur;
     int hauteur;
+    //Identifiant de la salle
     int id_salle;
+    //Dernier identifiant le plus élevé
     int id_max;
-    t_niveau * niveau;
+    t_salle * salleCourante = NULL;
+
+    t_niveau * niveau = malloc(sizeof(t_niveau));
+    if(niveau == NULL)
+    {
+        printf("Impossible d'allouer la mémoire d'un niveau.\n");
+        return NULL;
+    }
 
     //Lecture de la taille du niveau
     fscanf(fichier, "%i %i", &largeur, &hauteur);
-    
-    niveau = malloc(sizeof(t_niveau) * largeur * hauteur);
+
     niveau->l = largeur;
     niveau->h = hauteur;
 
@@ -22,14 +108,18 @@ int chargerNiveau(FILE * fichier)
     {
         for(int j = 0; j < largeur; j++)
         {
-            fscanf(fichier, "%i", id_salle);
-            if(id_salle == 0)
-                niveau->salles[i][j] = NULL;
+            fscanf(fichier, "%i", &id_salle);
+
+            if(id_salle == 0) //S'il n'y a pas de salle
+                niveau->salles[i*largeur + j] = NULL;
+            
             else
             {
                 if(id_salle > id_max) //Nouvelle salle
                 {
                     id_max = id_salle;
+                    salleCourante = malloc(sizeof(t_salle));
+                    
                 }
                 else //Groupe de salle
                 {
