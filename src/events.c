3080001386cc21eaf81784634cb2d5dfa4f81270
Guillaume Richard <guillaume.rwins@gmail.com>
2022-01-18 11:21:35 +0100

    Modification des fichiers sources et du Makefile

--- src/events.c
+++ src/events.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <SDL2/SDL.h>
 
 /*
@@ -8,34 +10,59 @@
 
 */
 
-void handleEvents(){
-    const char  * key_code = malloc(sizeof(char));
-    const char * key_name= malloc(sizeof(char));
+int handleEvents() {
+  const char * key_code;
+  const char * key_name;
 
-    
-    SDL_Event event;
+  int q_down = 0;
+  int ctrl_down = 0;
 
-    while (SDL_PollEvent(&event)) {
+  SDL_Event event;
+
+  while (SDL_PollEvent( & event)) {
 
     switch (event.type) {
-        case SDL_MOUSEBUTTONDOWN : printf("SDL_MOUSEBUTTONDOWN");;
-        case SDL_MOUSEBUTTONUP : printf("SDL_MOUSEBUTTONUP");;
-        case SDL_KEYDOWN : printf("SDL_KEYDOWN");
-                key_code = SDL_GetScancodeName(event.key.keysym.scancode),
-                key_name = SDL_GetKeyName(event.key.keysym.sym),
-                printf("La cle %s : %s a été appuyée", key_name,key_code);
-        case SDL_KEYUP : printf("SDL_KEYDOWN");
-                key_code = SDL_GetScancodeName(event.key.keysym.scancode),
-                key_name = SDL_GetKeyName(event.key.keysym.sym),
-                printf("La cle %s : %s a été relachée", key_name,key_code);
-
-        case SDL_MOUSEMOTION : printf("Mouse has moved");;
-            
+    case SDL_MOUSEBUTTONDOWN:
+      printf("SDL_MOUSEBUTTONDOWN");
+      break;
+    case SDL_MOUSEBUTTONUP:
+      printf("SDL_MOUSEBUTTONUP");
+      break;
+    case SDL_KEYDOWN:
+      printf("SDL_KEYDOWN"),
+      key_code = SDL_GetScancodeName(event.key.keysym.scancode),
+        key_name = SDL_GetKeyName(event.key.keysym.sym),
+        printf("La cle %s : %s a été appuyée", key_name, key_code);
+        switch (event.key.keysym.sym) {
+        case SDLK_q:
+          q_down = 1;
+          break;
+        case SDLK_LCTRL:
+          ctrl_down = 1;
+          break;;
+        };
+      break;
         
-
+    case SDL_KEYUP:
+      printf("SDL_KEYDOWN"),
+      key_code = SDL_GetScancodeName(event.key.keysym.scancode),
+        key_name = SDL_GetKeyName(event.key.keysym.sym),
+        printf("La cle %s : %s a été relachée", key_name, key_code);
+        switch (event.key.keysym.sym) {
+      case SDLK_q:
+        q_down = 0;
+        break;
+      case SDLK_LCTRL:
+        ctrl_down = 0;
+        break;
+      };break;
+        
+    case SDL_MOUSEMOTION:
+      printf("Mouse has moved");
+      break;
     }
 
-    }
-    key_code = NULL;
-    key_name = NULL;
+  if (q_down && ctrl_down)
+    return 1;
+  }
 }
