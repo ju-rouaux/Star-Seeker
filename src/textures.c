18c0e91c96557e5f4957e843735857d34ac7775a
Julien Rouaux <ju.rouaux@gmail.com>
2022-01-20 15:27:14 +0100

    Création du module textures permettant de charger toutes les textures dans une structure

---
+++ src/textures.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <textures.h>
+
+//REtourne NULL si echec
+t_textures * chargerTextures(SDL_Renderer * renderer)
+{
+    SDL_Surface * surface = NULL;
+
+    t_textures * textures = malloc(sizeof(t_textures));
+    if(textures == NULL)
+    {
+        printf("Impossible d'allouer la mémoire pour t_textures\n");
+        return NULL;
+    }
+
+    //Map
+    surface = SDL_LoadBMP("./img/tileset_map.bmp");
+    if(surface == NULL)
+    {
+        printf("Impossible de charger la surface tileset_map.bmp : %s\n", SDL_GetError());
+        free(textures);
+        return NULL;
+    }
+    textures->map = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if(textures->map == NULL)
+    {
+        printf("Impossible de charger la texture de tileset_map.bmp : %s\n", SDL_GetError());
+        free(textures);
+        return NULL;
+    }
+    surface = NULL;
+
+
+/*
+    //Player
+    surface = SDL_LoadBMP("./img/player.bmp");
+    if(surface == NULL)
+    {
+        printf("Impossible de charger la surface player.bmp : %s\n", SDL_GetError());
+        return NULL;
+    }
+    textures->map = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if(textures->map == NULL)
+    {
+        printf("Impossible de charger la texture de player.bmp : %s\n", SDL_GetError());
+        return NULL;
+    }
+    surface = NULL;
+
+*/
+
+    return 
+}
+
+void detruireTextures(t_textures ** textures)
+{
+    if(*textures != NULL)
+    {
+        if((*textures)->map != NULL)
+            SDL_DestroyTextures((*textures)->map);
+        if((*textures)->player != NULL)
+            SDL_DestroyTextures((*textures)->player);
+        free(*textures)
+    }
+    *textures = NULL;
+}
