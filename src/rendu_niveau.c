27ab2dabb23d88da70c22e2e37396cef70be6345
Julien Rouaux <ju.rouaux@gmail.com>
2022-01-26 10:41:50 +0100

    Update du rendu du niveau

--- src/rendu_niveau.c
+++ src/rendu_niveau.c
@@ -21,6 +21,9 @@
  */
 int dessinerTile(t_moteur * moteur, t_tile_type type, int x, int y, int taille)
 {
+    if(type == AUCUN)
+        return 0;
+
     SDL_Rect source; //Partie de du tileset à afficher
     SDL_Rect destination; //Position dans la fenetre où afficher
     
@@ -47,61 +50,167 @@ static int afficherSalle(t_moteur * moteur, t_salle * salle, int x, int y, int s
 {
     t_tile_type type;
     int id_courant = salle->id_salle;
-
-    //Afficher sol
-    for(int i = 0; i < NB_TILE_HAUTEUR; i++)
+    int i, j;
+    
+    //Dessiner sol
+    for(i = 0; i < NB_TILE_HAUTEUR; i++)
     {
-        for(int j = 0; j < NB_TILE_LARGEUR; j++)
+        for(j = 0; j < NB_TILE_LARGEUR; j++)
         {
-            if(i == 0) //Mur du haut
+            if(dessinerTile(moteur, SOL, (j+x)*scale, (i+y)*scale, scale) != 0)
+            {
+                printf("Erreur lors de l'affichage d'une tile du sol : %s\n", SDL_GetError());
+                return -1;
+            }
+        }
+    }
+    
+    //Dessiner mur haut
+    i = -1;
+    for(j = -1; j < NB_TILE_LARGEUR+1; j++)
+    {
+        if(salle->portes[UP] != NULL &&
+        id_courant == salle->portes[UP]->id_salle) //Si reliée en haut
+            type = AUCUN;
+        else
+        {
+            if(j == (NB_TILE_LARGEUR / 2) && salle->portes[UP] != NULL) //Porte haut
+                type = PORTE_HAUT;
+            else //Mur
+                type = MUR;
+                
+        }
+        if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
+        {
+            printf("Erreur lors de l'affichage d'une tile du mur du haut : %s\n", SDL_GetError());
+            return -1;
+        }
+    }
+    
+    //Dessiner mur bas
+    i = NB_TILE_HAUTEUR;
+    for(j = -1; j < NB_TILE_LARGEUR+1; j++)
+    {
+        if(salle->portes[DOWN] != NULL &&
+        id_courant == salle->portes[DOWN]->id_salle) //Si reliée en bas
+            type = AUCUN;
+        else
+        {
+            if(j == (NB_TILE_LARGEUR / 2) && salle->portes[DOWN] != NULL) //Porte bas
+                type = PORTE_BAS;
+            else //Mur
+                type = MUR;
+                
+        }
+        if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
+        {
+            printf("Erreur lors de l'affichage d'une tile du mur du bas : %s\n", SDL_GetError());
+            return -1;
+        }
+    }
+
+    //Dessiner mur gauche
+    j = -1;
+    for(i = -1; i < NB_TILE_HAUTEUR+1; i++)
+    {
+        if(salle->portes[LEFT] != NULL &&
+        id_courant == salle->portes[LEFT]->id_salle) //Si pas reliée à gauche
+            type = AUCUN;
+        else
+        {
+            if(i == (NB_TILE_HAUTEUR / 2) && salle->portes[LEFT] != NULL) //Porte gauche
+                type = PORTE_GAUCHE;
+            else //Mur
+                type = MUR;
+                
+            if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
+            {
+                printf("Erreur lors de l'affichage d'une tile du mur du bas : %s\n", SDL_GetError());
+                return -1;
+            }
+        }
+    }
+
+    //Dessiner mur droite
+    j = NB_TILE_LARGEUR;
+    for(i = -1; i < NB_TILE_HAUTEUR+1; i++)
+    {
+        if(salle->portes[RIGHT] != NULL &&
+        id_courant == salle->portes[RIGHT]->id_salle) //Si pas reliée à droite
+            type = AUCUN;
+        else
+        {
+            if(i == (NB_TILE_HAUTEUR / 2) && salle->portes[RIGHT] != NULL) //Porte droite
+                type = PORTE_DROITE;
+            else //Mur
+                type = MUR;
+                
+            if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
+            {
+                printf("Erreur lors de l'affichage d'une tile du mur du bas : %s\n", SDL_GetError());
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+
+    //ANCIEN RENDU
+    /*
+    //Afficher sol
+    for(int i = -1; i < NB_TILE_HAUTEUR+1; i++)
+    {
+        for(int j = -1; j < NB_TILE_LARGEUR+1; j++)
+        {
+            if(i == -1) //Mur du haut
             {
                 if(salle->portes[UP] != NULL &&
                 id_courant == salle->portes[UP]->id_salle &&
-                j != 0 && j != NB_TILE_LARGEUR-1) //Reliée en haut
-                    type = SOL;
+                j != -1 && j != NB_TILE_LARGEUR+1) //Reliée en haut
+                    type = AUCUN;
                 else
                 {
-                    if(j == (NB_TILE_LARGEUR / 2) && salle->portes[UP] != NULL) //Porte haut
+                    if(j == (NB_TILE_LARGEUR+2) / 2 && salle->portes[UP] != NULL) //Porte haut
                         type = PORTE_HAUT;
                     else //Mur
                         type = MUR;
                 }
             }
-            else if(i == NB_TILE_HAUTEUR-1) //Mur du bas
+            else if(i == NB_TILE_HAUTEUR+1) //Mur du bas
             {
                 if(salle->portes[DOWN] != NULL &&
                 id_courant == salle->portes[DOWN]->id_salle &&
-                j != 0 && j != NB_TILE_LARGEUR-1) //Reliée en haut
-                    type = SOL;
+                j != -1 && j != NB_TILE_LARGEUR+1) //Reliée en haut
+                    type = AUCUN;
                 else
                 {
-                    if(j == (NB_TILE_LARGEUR / 2) && salle->portes[DOWN] != NULL) //Porte bas
+                    if(j == (NB_TILE_LARGEUR+2) / 2 && salle->portes[DOWN] != NULL) //Porte bas
                         type = PORTE_BAS;
                     else //Mur
                         type = MUR;
                 }
             }
-            else if(j == 0) //Mur de gauche
+            else if(j == -1) //Mur de gauche
             {
                 if(salle->portes[LEFT] != NULL &&
                 id_courant == salle->portes[LEFT]->id_salle) //Reliée à gauche
-                    type = SOL;
+                    type = AUCUN;
                 else
                 {
-                    if(i == (NB_TILE_HAUTEUR / 2) && salle->portes[LEFT] != NULL) //Porte gauche
+                    if(i == (NB_TILE_HAUTEUR+2) / 2 && salle->portes[LEFT] != NULL) //Porte gauche
                         type = PORTE_GAUCHE;
                     else //Mur
                         type = MUR;
                 }
             }
-            else if(j == NB_TILE_LARGEUR-1) //Mur de droite
+            else if(j == NB_TILE_LARGEUR+1) //Mur de droite
             {
                 if(salle->portes[RIGHT] != NULL &&
                 id_courant == salle->portes[RIGHT]->id_salle) //Reliée à gauche
-                    type = SOL;
+                    type = AUCUN;
                 else
                 {
-                    if(i == (NB_TILE_HAUTEUR / 2) && salle->portes[RIGHT] != NULL) //Porte gauche
+                    if(i == (NB_TILE_HAUTEUR+2) / 2 && salle->portes[RIGHT] != NULL) //Porte gauche
                         type = PORTE_DROITE;
                     else //Mur
                         type = MUR;
@@ -109,16 +218,15 @@ static int afficherSalle(t_moteur * moteur, t_salle * salle, int x, int y, int s
             }
             else
                 type = SOL;
-
-            if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
-            {
-                printf("Erreur lors de l'affichage d'une tile : %s\n", SDL_GetError());
-                return -1;
-            } 
+            if(type != AUCUN)
+                if(dessinerTile(moteur, type, (j+x)*scale, (i+y)*scale, scale) != 0)
+                {
+                    printf("Erreur lors de l'affichage d'une tile : %s\n", SDL_GetError());
+                    return -1;
+                } 
         }
     }
-
-    return 0;
+    */
 }
 
 //Fonction temporaire affichant l'entièreté du niveau.
@@ -126,15 +234,15 @@ static int afficherSalle(t_moteur * moteur, t_salle * salle, int x, int y, int s
 int afficherNiveau(t_moteur * moteur, t_niveau * niveau)
 {
     int resultat = 0;
+    int id_desire = 25;
     //Calculer camera et ainsi calculer coordonnées origine du niveau relatives caméra
     for(int i = 0; i < niveau->h; i++)
     {
         for(int j = 0; j < niveau->l; j++)
         {
             if(niveau->salles[i*niveau->l + j] != NULL)
-                resultat = afficherSalle(moteur, niveau->salles[i*niveau->l + j], j*NB_TILE_LARGEUR, i*NB_TILE_HAUTEUR, 4);
-            
-            
+                if(niveau->salles[i*niveau->l +j]->id_salle == id_desire)
+                    resultat = afficherSalle(moteur, niveau->salles[i*niveau->l + j], j*NB_TILE_LARGEUR, i*NB_TILE_HAUTEUR, 6);
         }
     }
     return 0;
