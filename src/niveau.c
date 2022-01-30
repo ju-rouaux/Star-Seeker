ca74cfb324024d497ca818e9133d5688bc7e72d9
Julien Rouaux <ju.rouaux@gmail.com>
2022-01-30 13:58:34 +0100

    Ajout des dimensions des salles, et finalisation des commentaires

--- src/niveau.c
+++ src/niveau.c
@@ -1,6 +1,6 @@
 /**
  * \file
- * \brief Module de chargement d'un niveau en structure interprétable pour notre moteur de rendu. 
+ * \brief Module de chargement d'un niveau en structure interprétable pour le jeu.
  * 
  * \author Julien
  */
@@ -9,7 +9,22 @@
 #include <stdlib.h>
 #include <niveau.h>
 
-t_dimensions_salle * creerDimensions(int i, int j)
+
+//A faire
+//static void chargerMonstres();
+//static void chargerObstacles();
+//static void chargerSalle(); //Appelle les 2 précédentes
+
+
+/**
+ * \brief Génère une structure dimensions de salle.
+ * 
+ * \param i Coordonnée en hauteur, de la salle d'origine dans la matrice niveau
+ * \param j Coordonnée en largeur, de la salle d'origine dans la matrice niveau
+ * 
+ * \return Le pointeur des dimensions, NULL si echec.
+ */
+static t_dimensions_salle * creerDimensions(int i, int j)
 {
     t_dimensions_salle * dimensions = malloc(sizeof(t_dimensions_salle));
     if(dimensions == NULL)
@@ -27,25 +42,35 @@ t_dimensions_salle * creerDimensions(int i, int j)
     return dimensions;
 }
 
-/**
- * Ne fonctionne que si une salle ne pointe sur la dimension
- * 
- */
-void detruireDimensions(t_dimensions_salle ** dimensions)
-{
-    if(dimensions != NULL && (*dimensions)->nombre == 1)
-        free(*dimensions);
-    dimensions = NULL;
-}
 
 /**
- * Remplie la matrice t_salle * portes[] du pointeur des salles voisines de chaque salles. 
+ * \brief Libère la mémoire seulement si plus aucune salle ne pointe sur la dimension et mets son pointeur à NULL.
+ * 
+ * C'est à dire si le champ nombre vaut 0.
+ * 
+ * \param dimensions L'adresse du pointeur de la dimension
+ */
+static void detruireDimensions(t_dimensions_salle ** dimensions)
+{
+    if(*dimensions != NULL && (*dimensions)->nombre == 0)
+        free(*dimensions);
+
+    *dimensions = NULL;
+}
+
+
+/**
+ * \brief Remplie la matrice t_salle * portes[] du pointeur des salles voisines de chaque salles, 
+ * et génère les dimensions de chaque salle.
+ * 
  * Fonctionnement : 
  *      Pour chaque salle, si une voisine existe, il faut lier les portes des salles. 
  *      Sinon ne rien faire, pour que les portes pointent toujours vers NULL.
  *      Deux salles liées et ayant un même identifiant ne formeront qu'une salle.
+ *      Si deux salles liées ne forment qu'une salle, actualiser les dimensions et
+ *      faire pointer les salles sur la même structure dimensions.
  * 
- *  \param niveau Le niveau dont il faut lier les salles
+ * \param niveau Le niveau dont il faut lier les salles
  * 
  * \return 0 si succès. Valeur négative si échec.
  */
@@ -53,37 +78,50 @@ static int lierSalles(t_niveau * niveau)
 {   
     t_salle * salle_courante = NULL;
     int id_courant;
+    int liee = 0; //Vrai si la salle courante a été liée
 
     for(int i = 0; i < niveau->h; i++)
     {
         for(int j = 0; j < niveau->l; j++)
         {
             salle_courante = niveau->salles[i*niveau->l + j];
-            id_courant = salle_courante->id_salle;
 
             if(salle_courante != NULL) //Si la salle courante existe
             {
+                id_courant = salle_courante->id_salle;
                 if( i > 0 && //Pour éviter de sortir du haut de la matrice
                     niveau->salles[(i-1)*niveau->l + j] != NULL) //Si la salle du dessus existe
                 {
-                    //Lier porte haut salle courante à celle du bas salle du dessus
+                    //Lier porte haut salle courante à celle du bas de la salle du dessus
                     salle_courante->portes[UP] = niveau->salles[(i-1)*niveau->l + j];
                     niveau->salles[(i-1)*niveau->l + j]->portes[DOWN] = salle_courante;
+                    //Lier les dimensions
+                    if(salle_courante->id_salle == salle_courante->portes[UP]->id_salle)
+                    {
+                        liee++;
+                        salle_courante->dimensions = salle_courante->portes[UP]->dimensions;
+                        if(i > salle_courante->dimensions->i + salle_courante->dimensions->hauteur - 1)
+                            (salle_courante->dimensions->hauteur)++;
+                    }
                 }
 
                 if( j > 0 && //Pour éviter de sortir d'à gauche de la matrice
                     niveau->salles[i*niveau->l + (j-1)] != NULL) //Si la salle de gauche existe
                 {
-                    //Lier porte gauche salle courante à celle de droite salle de gauche
+                    //Lier porte gauche salle courante à celle de droite de la salle de gauche
                     salle_courante->portes[LEFT] = niveau->salles[i*niveau->l + (j-1)];
                     niveau->salles[i*niveau->l + (j-1)]->portes[RIGHT] = salle_courante;
+                    //Lier les dimensions
+                    if(salle_courante->id_salle == salle_courante->portes[LEFT]->id_salle)
+                    {
+                        liee++;
+                        salle_courante->dimensions = salle_courante->portes[LEFT]->dimensions;
+                        if(j > salle_courante->dimensions->j + salle_courante->dimensions->largeur - 1)
+                            (salle_courante->dimensions->largeur)++;
+                    }
                 }
                 
-/*              Ce code pose problème. Mince !
-
-                //-- Calcul des dimensions des salles --
-                if(id_courant != salle_courante->portes[UP]->id_salle &&
-                id_courant != salle_courante->portes[LEFT]->id_salle) //Salle origine
+                if( !liee )
                 {
                    salle_courante->dimensions = creerDimensions(i, j);
                    if(salle_courante->dimensions == NULL)
@@ -91,37 +129,20 @@ static int lierSalles(t_niveau * niveau)
                        printf("La liaison des salles a échoué\n");
                        return -1;
                    }
-                }
-                else //Extension de la salle
-                {
-                    if(id_courant == salle_courante->portes[UP]->id_salle)
-                    {
-                        salle_courante->dimensions = salle_courante->portes[UP]->dimensions;
-                        if(i > salle_courante->dimensions->i + salle_courante->dimensions->hauteur - 1)
-                            salle_courante->dimensions->hauteur++;
-                    }
+                } 
+                else
+                    (salle_courante->dimensions->nombre)++;
 
-                    if(id_courant == salle_courante->portes[LEFT]->id_salle)
-                    {
-                        salle_courante->dimensions = salle_courante->portes[LEFT]->dimensions; //Peut écraser l'affectation du if d'au dessus mais c'est pas grave c'est la même valeur
-                        salle_courante->dimensions = salle_courante->portes[LEFT]->dimensions;
-                        if(j > salle_courante->dimensions->j + salle_courante->dimensions->largeur - 1)
-                            salle_courante->dimensions->largeur++;
-                    }
-
-                    salle_courante->dimensions->nombre++;
-                }*/
+                liee = 0;
             }
         }
     }
-
     return 0;
 }
 
 
 /**
- * Génère une structure salle et mets les pointeurs de son groupe 
- * à NULL.
+ * \brief Génère une structure salle et mets les pointeurs de porte à NULL.
  * 
  * \param id_salle L'identifiant de la salle
  * 
@@ -141,14 +162,14 @@ static t_salle * creerSalle(int id_salle)
 
     salle->complete = 0;
     salle->id_salle = id_salle;
+    salle->dimensions = NULL;
 
     return salle;
 }
 
 
 /**
- * Libère la mémoire allouée pour une salle et mets son 
- * pointeur à NULL. 
+ * \brief Libère la mémoire allouée pour une salle et mets son pointeur à NULL. 
  * 
  * \param salle L'adresse du pointeur de la salle
  */
@@ -156,23 +177,19 @@ static void detruireSalle(t_salle ** salle)
 {
     if(*salle != NULL)
     {
-        --(*salle)->dimensions->nombre;
+        ((*salle)->dimensions->nombre)--; //Nombre vaut ainsi 0 si c'est la dernière salle
         detruireDimensions(&(*salle)->dimensions);
+        free((*salle)->dimensions);
         free(*salle);
     }
-    salle = NULL;
+    *salle = NULL;
 }
-
-//A faire
-//static void chargerMonstres();
-//static void chargerObstacles();
-//static void chargerSalle(); //Appelle les 2 précédentes
 
 
 /**
- * Charge le niveau dans une structure exploitable pour le jeu.
+ * \brief Charge le niveau dans une structure exploitable pour le jeu.
  * 
- * \param fichier Fichier depuis lequel charger le niveau
+ * \param fichier Fichier depuis lequel charger le niveau (flux)
  * 
  * \return Le pointeur du niveau chargé, NULL si echec du chargement.
  */
@@ -181,11 +198,12 @@ t_niveau * chargerNiveau(FILE * fichier)
     //Dimensions du niveau
     int largeur;
     int hauteur;
+
     //Identifiant de salle
     int id_salle;
 
-    t_niveau * niveau = malloc(sizeof(t_niveau));
     t_salle * salleCourante = NULL;
+    t_niveau * niveau = malloc(sizeof(t_niveau));
 
     if(niveau == NULL)
     {
@@ -195,11 +213,15 @@ t_niveau * chargerNiveau(FILE * fichier)
 
     //Lecture de la taille du niveau
     fscanf(fichier, "%i %i", &largeur, &hauteur);
-    printf("l : %i / h : %i\n", largeur, hauteur);
-
     niveau->l = largeur;
     niveau->h = hauteur;
+
     niveau->salles = malloc(sizeof(t_salle*)*hauteur*largeur); //Allouer l'array de pointeurs
+    if(niveau->salles == NULL)
+    {
+        printf("Impossible d'allouer la mémoire pour la matrice de salle.\n");
+        return NULL;
+    }
 
     for(int i = 0; i < hauteur; i++)
     {
@@ -207,7 +229,7 @@ t_niveau * chargerNiveau(FILE * fichier)
         {
             fscanf(fichier, "%i", &id_salle);
 
-            if(feof(fichier)) //En cas d'erreur du fichier, ne rien charger de particulier
+            if(feof(fichier)) //Si on est déjà à la fin du fichier, ne rien charger de particulier
                 id_salle = 0;
 
             if(id_salle == 0) //S'il n'y a pas de salle
@@ -236,8 +258,7 @@ t_niveau * chargerNiveau(FILE * fichier)
 }
 
 /**
- * Libère la mémoire allouée pour un niveau et mets son 
- * pointeur à NULL
+ * \brief Libère la mémoire allouée pour un niveau et mets son pointeur à NULL.
  * 
  * \param niveau L'adresse du pointeur du niveau
  */
@@ -247,9 +268,11 @@ void detruireNiveau(t_niveau ** niveau)
     {
         for(int i = 0; i < (*niveau)->h; i++)
             for(int j = 0; j < (*niveau)->l; j++)
-                detruireSalle(&(*niveau)->salles[i*(*niveau)->l + j]); //La fonction appelée se charge de vérifier si le pointeur n'est pas NULL
+                if((*niveau)->salles[i*(*niveau)->l + j] != NULL)
+                    detruireSalle(&(*niveau)->salles[i*(*niveau)->l + j]);
         free((*niveau)->salles);
         free(*niveau);
     }
-    niveau = NULL;
+
+    *niveau = NULL;
 }
