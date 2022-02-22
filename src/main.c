cf4996884e0c7e4dc52c15db7b6f9e2f010fa2af
Guillaume Richard <guillaume.rwins@gmail.com>
2022-02-22 15:50:39 +0100

    essaie

--- src/main.c
+++ src/main.c
@@ -3,36 +3,37 @@
 #include <SDL2/SDL.h>
 #include <window.h>
 #include <generation_niveau.h>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-int main(int argc, char * argv[])
+#include <sauvegarde.h>
+
+void main ()
 {
 
+    t_salle * sallein = malloc(sizeof(t_salle));
+    t_dimensions_salle * dimension = malloc(sizeof(t_dimensions_salle));
 
-    
-    char * nom_planete = "bloblop";
-    creer_niveau("test15.niv", nom_planete);
-
-
+    t_salle salleout;
 
 
+    sallein->complete = 1;
+    sallein->dimensions = dimension;
+    sallein->dimensions->hauteur = 20;
+    sallein->dimensions->i = 10;
+    sallein->dimensions->j = 10;
+    sallein->dimensions->largeur = 20;
+    sallein->dimensions->nombre = 5;
+    sallein->id_salle = 1;
+//  sallein->portes[0].
+
+
+    char * filename = "save.txt";
+
+
+    write_file_struct_salle(filename,sallein);
+    salleout = *read_file_struct_salle(filename);
+
+    printf("\nsallein->complete = %d, \nsallein->dimensions->hauteur = %d, \nsallein->dimensions->i = %d, \nsallein->dimensions->j = %d, \nsallein->dimensions->largeur = %d, \nsallein->dimensions->nombre = %d, \nsallein->id_salle = %d",sallein->complete, sallein->dimensions->hauteur, sallein->dimensions->i, sallein->dimensions->j, sallein->dimensions->largeur, sallein->dimensions->nombre, sallein->id_salle);
+    // printf("id_salle = %d, complete = %d, chaine = %s",salleout.id_salle,salleout.complete,salleout.str);
 
 
 
-    SDL_Window * window = NULL;
-    SDL_Renderer * renderer = NULL;
-    
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        printf("Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-
-
-    creerFenetreEtRendu(&window, &renderer);
-    SDL_Delay(5000);
-    detruireFenetreEtRendu(&window, &renderer);
-
-
-    SDL_Quit();
-    return 0;
 }
--- src/sauvegarde.c
+++ src/sauvegarde.c
@@ -1,5 +1,12 @@
 #include "sauvegarde.h"
 
+
+/**
+ * \brief Ecrit dans un fichier en binaire l'etat des la structure t_salle
+ *
+ * \param filename nom de fichier de sauvegarde
+ * \param input strcuture a sauvegarder
+ */
 void write_file_struct_salle(char * filename, t_salle * input){
 
     FILE *outfile;
@@ -26,7 +33,12 @@ void write_file_struct_salle(char * filename, t_salle * input){
 
 }
 
-
+/**
+ * \brief Lis dans un fichier en binaire l'etat des la structure t_salle
+ *
+ * \param filename nom de fichier de sauvegarde
+ * \return t_salle* la structure lu dans le fichier
+ */
 t_salle * read_file_struct_salle(char * filename){
     FILE *infile;
 
@@ -49,40 +61,6 @@ t_salle * read_file_struct_salle(char * filename){
     fclose (infile);
 
     return tmp;
-
-}
-
-// Driver program
-void main ()
-{
-
-    t_salle * sallein = malloc(sizeof(t_salle));
-    t_dimensions_salle * dimension = malloc(sizeof(t_dimensions_salle));
-
-    t_salle salleout;
-
-
-    sallein->complete = 1;
-    sallein->dimensions = dimension;
-    sallein->dimensions->hauteur = 20;
-    sallein->dimensions->i = 10;
-    sallein->dimensions->j = 10;
-    sallein->dimensions->largeur = 20;
-    sallein->dimensions->nombre = 5;
-    sallein->id_salle = 1;
-//  sallein->portes[0].
-
-
-    char * filename = "save.txt";
-
-
-    write_file_struct_salle(filename,sallein);
-    salleout = *read_file_struct_salle(filename);
-
-    printf("\nsallein->complete = %d, \nsallein->dimensions->hauteur = %d, \nsallein->dimensions->i = %d, \nsallein->dimensions->j = %d, \nsallein->dimensions->largeur = %d, \nsallein->dimensions->nombre = %d, \nsallein->id_salle = %d",sallein->complete, sallein->dimensions->hauteur, sallein->dimensions->i, sallein->dimensions->j, sallein->dimensions->largeur, sallein->dimensions->nombre, sallein->id_salle);
-    // printf("id_salle = %d, complete = %d, chaine = %s",salleout.id_salle,salleout.complete,salleout.str);
-
-
 
 }
 
