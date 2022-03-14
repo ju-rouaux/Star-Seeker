eee42e9108252ef93575f40982acecb4d2fbb475
Guillaume Richard <guillaume.rwins@gmail.com>
2022-03-14 11:57:44 +0100

    Modif commentaire doxygen

--- src/events.c
+++ src/events.c
@@ -19,9 +19,9 @@
 
 int handleEvents(t_joueur * joueur, t_moteur * moteur) {
 
-    //const char * key_code, * key_name; /**nom de la touche azerty, qwerty*/
-    //int mouse_x, mouse_y; /**Coordonnées du curseur*/
-    int scrolling; /**Boléen, 1 si on scrolle vers le haut, 0 si on scrolle vers le bas*/
+    //const char * key_code, * key_name; /*nom de la touche azerty, qwerty*/
+    //int mouse_x, mouse_y; /*Coordonnées du curseur*/
+    int scrolling; /*Boléen, 1 si on scrolle vers le haut, 0 si on scrolle vers le bas*/
 
     SDL_Event event;
     while(SDL_PollEvent(&event))
@@ -31,8 +31,8 @@ int handleEvents(t_joueur * joueur, t_moteur * moteur) {
             case SDL_QUIT:
                 return 1;
             
-            /**gestion de la souris*/
-            /**Si un des boutons de la souris est appuyé*/
+            /*gestion de la souris*/
+            /*Si un des boutons de la souris est appuyé*/
             case SDL_MOUSEBUTTONDOWN:
                 switch (event.button.button)
                 {
@@ -50,30 +50,30 @@ int handleEvents(t_joueur * joueur, t_moteur * moteur) {
                         break;
                 };
                 switch (event.button.clicks)
-                { /**Nombre de clics*/
-                    case 1: /**Clic simple*/
+                { /*Nombre de clics*/
+                    case 1: /*Clic simple*/
                         //printf("\nSimple click");
                         break;
-                    case 2: /**Double clic*/
+                    case 2: /*Double clic*/
                         //printf("\nDouble click");
                         break;
-                    default: /**Si plus qu'un double clic*/
+                    default: /*Si plus qu'un double clic*/
                         printf("\nClicked too many times hahaha");
                         break;
                 };
                 break;
 
-             /**Si un des boutons de la souris est relaché*/
+             /*Si un des boutons de la souris est relaché*/
             case SDL_MOUSEBUTTONUP:
                 switch (event.button.button)
                 {
-                    case SDL_BUTTON_LEFT: /**Bouton gauche*/
+                    case SDL_BUTTON_LEFT: /*Bouton gauche*/
                         //printf("\nMouse left button released");
                         break;
-                    case SDL_BUTTON_MIDDLE: /**Molette*/
+                    case SDL_BUTTON_MIDDLE: /*Molette*/
                         //printf("\nMouse scroll button released");
                         break;
-                    case SDL_BUTTON_RIGHT: /**Bouton droit*/
+                    case SDL_BUTTON_RIGHT: /*Bouton droit*/
                         printf("\nMouse right button released");
                         break;
                     default:
@@ -83,11 +83,11 @@ int handleEvents(t_joueur * joueur, t_moteur * moteur) {
                 break;
             case SDL_MOUSEMOTION:
                 //mouse_x = event.button.x, mouse_y = event.button.y;
-                /**Recupere les coordonées x et y relative a la fenetre*/
+                /*Recupere les coordonées x et y relative a la fenetre*/
                 //printf("\nMouse has moved : Mouse coordinates relative to window : x = %d, y = %d", mouse_x, mouse_y);
                 break;
             case SDL_MOUSEWHEEL:
-                scrolling = event.wheel.y; /**Recupere la valeur de la molette, 1 si on scrolle vers le haut, 0 si on scrolle vers le bas */
+                scrolling = event.wheel.y; /*Recupere la valeur de la molette, 1 si on scrolle vers le haut, 0 si on scrolle vers le bas */
                 if (scrolling < 0)
                     //printf("\nScrolling down");
                 if (scrolling > 0)
@@ -120,7 +120,7 @@ int handleEvents(t_joueur * joueur, t_moteur * moteur) {
                         break;
                 }
                 break;
-            case SDL_KEYUP: /**touche relachée*/
+            case SDL_KEYUP: /*touche relachée*/
                 switch (event.key.keysym.scancode)
                 {
                     case SDL_SCANCODE_W: //Z
@@ -150,13 +150,13 @@ int handleEvents(t_joueur * joueur, t_moteur * moteur) {
             case SDL_WINDOWEVENT:
                 switch (event.window.event)
                 {
-                    case SDL_WINDOWEVENT_SHOWN: /**Fenetre montrée*/
+                    case SDL_WINDOWEVENT_SHOWN: /*Fenetre montrée*/
                         printf("\nWindow shown");
                         break;
-                    case SDL_WINDOWEVENT_HIDDEN: /**Fenetre cachée*/
+                    case SDL_WINDOWEVENT_HIDDEN: /*Fenetre cachée*/
                         printf("\nWindow hidden");
                         break;
-                    case SDL_WINDOWEVENT_MAXIMIZED: /**Fenetre maximisée*/
+                    case SDL_WINDOWEVENT_MAXIMIZED: /*Fenetre maximisée*/
                         printf("\nWindow maximized");
                         break;
                     case SDL_WINDOWEVENT_MINIMIZED: /**Fenetre minimisée*/
