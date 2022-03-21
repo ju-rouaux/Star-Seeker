0c8cd4d35ea34a93338077d35f449fb07eef9589
Dakire <62287825+Dakire@users.noreply.github.com>
2022-03-21 21:57:24 +0100

    J'ai oublié les modifs(modifs mineures)

--- include/definitions/definitions_projectiles.h
+++ include/definitions/definitions_projectiles.h
@@ -90,16 +90,18 @@ static int updateProjectile_RetourProj(t_moteur * moteur, t_projectile * project
     if(projectile->duree_de_vie <= 0)
         return -1;
 
-    if((projectile->x < x+0.5 && projectile->x > x-0.5) && (projectile->y < y+0.5 && projectile->y > y-0.5) && (projectile->duree_de_vie < 2900))
+    if((projectile->x < x+0.5 && projectile->x > x-0.5) && (projectile->y < y+0.5 && projectile->y > y-0.5) && (projectile->duree_de_vie < 1900))
         return -1;
     
-    if(projectile->duree_de_vie < 4*3000/5){
+    if(projectile->duree_de_vie < 4*2000/5){
         projectile->direction_vx = x - projectile->x;
         projectile->direction_vy = y - projectile->y;
-    }
+        projectile->vitesse = 12;
+    }   
     if(deplacerEntite(moteur, (t_entite*) projectile) == -1){
         projectile->direction_vx = x - projectile->x;
         projectile->direction_vy = y - projectile->y;
+    
     }
     return 0;
 }
@@ -214,9 +216,9 @@ static int proj_sabre(t_projectile * projectile)
     projectile->id_animation = 4;
 
     projectile->taille = 1;
-    projectile->vitesse = 12;
+    projectile->vitesse = 15;
     projectile->dommages = 30;
-    projectile->duree_de_vie = 3000;
+    projectile->duree_de_vie = 2000;
 
     projectile->update = (int (*)(t_moteur *, t_entite *, float, float)) updateProjectile_RetourProj;
 
@@ -237,9 +239,9 @@ static int proj_laser(t_projectile * projectile)
     projectile->id_animation = 5;
 
     projectile->taille = 1;
-    projectile->vitesse = 12;
+    projectile->vitesse = 5;
     projectile->dommages = 30;
-    projectile->duree_de_vie = 3000;
+    projectile->duree_de_vie = 2500;
 
     projectile->update = (int (*)(t_moteur *, t_entite *, float, float)) updateProjectile_RetourProj;
 
