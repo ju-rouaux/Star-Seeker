bc490045e80f54f81cff09bff9c1b441d0fa8ae8
Julien Rouaux <ju.rouaux@gmail.com>
2022-03-27 20:59:12 +0200

    Correction fuites et commentaires sur menu_niveau.c

--- include/menu_niveau.h
+++ include/menu_niveau.h
@@ -1,4 +1,17 @@
+/**
+ * \file menu_niveau.h
+ * 
+ * \brief Menu de sélection d'un niveau d'une partie.
+ * 
+ * \author Julien Rouaux
+ */
+
+#ifndef _JEU_MENU_NIVEAU_
+#define _JEU_MENU_NIVEAU_
+
 #include <moteur.h>
 #include <generation_niveau.h>
 
 e_code_main afficherMenuNiveau(int * retour, t_moteur * moteur, niveau_informations_t ** infos_niveaux, int nb_infos);
+
+#endif //_JEU_MENU_NIVEAU_
--- src/menu_niveau.c
+++ src/menu_niveau.c
@@ -1,9 +1,26 @@
+/**
+ * \file menu_niveau.c
+ * 
+ * \brief Menu de sélection d'un niveau d'une partie.
+ * 
+ * \author Julien Rouaux
+ */
+
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <generation_niveau.h>
 #include <main.h>
+#include <moteur.h>
+#include <generation_niveau.h>
 
 
+/**
+ * \struct t_selection
+ * \brief Représente un choix sur le menu de sélection de niveau.
+ * 
+ * La sélection se compose d'un texte : le nom de la planète,
+ * et de l'image d'une planète à l'échelle du niveau.
+ */
 typedef struct
 {
     int indice;
@@ -19,7 +36,15 @@ typedef struct
 } t_selection;
 
 
-//Retourne -1 si rien survolé, sinon indice sélection
+/**
+ * \brief Retourne l'indice du niveau étant survolé par la souris.
+ * 
+ * \param selections La liste différentes options du menu
+ * \param nb_selects Le nombre de sélections différentes
+ * \param souris La position du curseur de la souris
+ * 
+ * \return L'indice du niveau survolé par la souris, ou -1 si aucun.
+ */
 static int indiceSelection(t_selection ** selections, int nb_selects, SDL_Point * souris)
 {
     for(int i = 0; i < nb_selects; i++)
@@ -30,11 +55,20 @@ static int indiceSelection(t_selection ** selections, int nb_selects, SDL_Point 
 }
 
 
+/**
+ * \brief Affiche une sélection du menu en fonction de son indice.
+ * 
+ * \param moteur Le moteur
+ * \param select La sélection à afficher
+ * \param apparence L'apparence de la planète affichée sur la sélection
+ * \param quantite Le nombre de sélections total qui sera affiché sur l'écran
+ */
 static void renduSelection(t_moteur * moteur, t_selection * select, SDL_Texture * apparence, int quantite)
 {
-    SDL_SetTextureColorMod(select->rendu_texte, 255,255,255);
-    int x = moteur->window_width / 4 + (select->indice * (moteur->window_width - moteur->window_width / 4) / quantite);
+    //Calcul de la position de la sélection
+    int x = moteur->window_width / 5 + (select->indice * (moteur->window_width - moteur->window_width / 5) / quantite);
     int y = moteur->window_height / 2;
+
     //Rendu planète
     select->position_planete.h = moteur->echelle * select->taille_planete;
     select->position_planete.w = moteur->echelle * select->taille_planete;
@@ -51,7 +85,17 @@ static void renduSelection(t_moteur * moteur, t_selection * select, SDL_Texture 
 }
 
 
-
+/**
+ * \brief Créer un bouton pour sélectionner un niveau.
+ * 
+ * \param renderer Le renderer du jeu
+ * \param police La police d'affichage du nom de la planète
+ * \param texte Le nom de la planète
+ * \param indice L'indice du niveau
+ * \param taille_planete La taille d'affichage de la planète
+ * 
+ * \return Le bouton, NULl si echec.
+ */
 static t_selection * creerSelection(SDL_Renderer * renderer, TTF_Font * police, char * texte, int indice, float taille_planete)
 {
     t_selection * select = malloc(sizeof(t_selection));
@@ -92,6 +136,12 @@ static t_selection * creerSelection(SDL_Renderer * renderer, TTF_Font * police, 
 }
 
 
+/**
+ * \brief Libère la mémoire allouée à la liste de sélections du menu.
+ * 
+ * \param selections Adresse de la liste des sélections à libérer
+ * \param nb_selections Nombre d'éléments dans la liste
+ */
 void detruireSelections(t_selection *** selections, int nb_selections)
 {
     if(*selections != NULL)
@@ -110,6 +160,16 @@ void detruireSelections(t_selection *** selections, int nb_selections)
     *selections = NULL;
 }
 
+
+/**
+ * \brief Alloue la liste infos_niveau les différents choix du menu.
+ * 
+ * \param moteur Le moteur
+ * \param infos_niveaux La liste des informations sur la structure des niveaux de la partie
+ * \param nb_infos Le nombre d'éléments de la liste
+ * 
+ * \return La liste des différents choix contextuels du menu.
+ */
 t_selection ** initSelections(t_moteur * moteur, niveau_informations_t ** infos_niveaux, int nb_infos)
 {
     //Matrice des sélections possibles du menu
@@ -142,7 +202,16 @@ t_selection ** initSelections(t_moteur * moteur, niveau_informations_t ** infos_
 }
 
 
-
+/**
+ * \brief Affiche le menu de sélection d'une planète.
+ * 
+ * \param retour_niveau Valeur de sortie, retourne l'indice du niveau à charger si un choix est fait
+ * \param moteur Le moteur
+ * \param infos_niveaux La liste des informations sur la structure des niveaux de la partie
+ * \param nb_infos Le nombre d'éléments de la liste
+ * 
+ * \return L'action réalisée, l'indice du niveau choisi est retourné dans retour_niveau si NIVEAU_CHANGER est retourné.
+ */
 e_code_main afficherMenuNiveau(int * retour_niveau, t_moteur * moteur, niveau_informations_t ** infos_niveaux, int nb_infos)
 {
     SDL_Event event;
@@ -168,6 +237,7 @@ e_code_main afficherMenuNiveau(int * retour_niveau, t_moteur * moteur, niveau_in
             switch(event.type)
             {
                 case SDL_QUIT:
+                    detruireSelections(&selections, nb_infos);
                     return JEU_QUITTER;
                 
                 case SDL_KEYDOWN:
@@ -199,9 +269,15 @@ e_code_main afficherMenuNiveau(int * retour_niveau, t_moteur * moteur, niveau_in
         for(int i = 0; i < nb_infos; i++)
         {
             if(indice_selection == i)
+            {
+                SDL_SetTextureColorMod(selections[i]->rendu_texte, 255,0,0);
                 SDL_SetTextureColorMod(moteur->textures->planete, 255,0,0);
+            }
             else
+            {
+                SDL_SetTextureColorMod(selections[i]->rendu_texte, infos_niveaux[i]->rouge, infos_niveaux[i]->vert, infos_niveaux[i]->bleu);
                 SDL_SetTextureColorMod(moteur->textures->planete, infos_niveaux[i]->rouge, infos_niveaux[i]->vert, infos_niveaux[i]->bleu);
+            }
             renduSelection(moteur, selections[i], moteur->textures->planete, nb_infos);
         }
         indice_selection = indiceSelection(selections, nb_infos, &souris);
